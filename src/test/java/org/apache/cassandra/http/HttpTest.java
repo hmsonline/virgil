10daeed30bb805ffe9259245e8d78a24d6a43d45
boneill42 <bone@alumni.brown.edu>
2011-11-10 19:15:14 +0000

    Fix for issue 3
    Adding PATCH method to support updating a row.
    Should be revisited when @Patch is available in JAX-RS.
    For now, created our own annotation.
    
    
    
    git-svn-id: https://svn.codespot.com/a/apache-extras.org/virgil/trunk@30 d6d3f0c6-2a50-3b37-6bf7-63741bc15edd

--- src/test/java/org/apache/cassandra/http/HttpTest.java
+++ src/test/java/org/apache/cassandra/http/HttpTest.java
@@ -2,6 +2,7 @@ package org.apache.cassandra.http;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.cassandra.http.ext.PatchMethod;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.DeleteMethod;
@@ -50,77 +51,77 @@ public class HttpTest {
 		this.send(client, put, 204);
 
 		// INSERT ROW
-		put = new PutMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY);
-		RequestEntity requestEntity = new StringRequestEntity(
-				"{\"ADDR1\":\"1234 Fun St.\",\"CITY\":\"Souderton.\"}",
+		put = new PutMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
+		RequestEntity requestEntity = new StringRequestEntity("{\"ADDR1\":\"1234 Fun St.\",\"CITY\":\"Souderton.\"}",
 				"appication/json", "UTF8");
 		put.setRequestEntity(requestEntity);
 		this.send(client, put, 204);
 
 		// FETCH ROW (VERIFY ROW INSERT)
-		GetMethod get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY
-				+ "/" + KEY);
+		GetMethod get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
 		this.send(client, get, 200);
 		String body = get.getResponseBodyAsString();
-		assertEquals("{\"ADDR1\":\"1234 Fun St.\",\"CITY\":\"Souderton.\"}",
-				body);
+		assertEquals("{\"ADDR1\":\"1234 Fun St.\",\"CITY\":\"Souderton.\"}", body);
 		logger.debug(body);
 
+		// PATCH ROW
+		PatchMethod patch = new PatchMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
+		requestEntity = new StringRequestEntity("{\"ADDR1\":\"1235 Fun St.\",\"COUNTY\":\"Montgomery\"}",
+				"appication/json", "UTF8");
+		patch.setRequestEntity(requestEntity);
+		this.send(client, patch, 204);
+		
+		// FETCH ROW (VERIFY PATCH)
+		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
+		this.send(client, get, 200);
+		body = get.getResponseBodyAsString();
+		assertEquals("{\"ADDR1\":\"1235 Fun St.\",\"COUNTY\":\"Montgomery\",\"CITY\":\"Souderton.\"}", body);
+		logger.debug(body);
+		
 		// INSERT COLUMN
-		put = new PutMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY + "/STATE/");
+		put = new PutMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY + "/STATE/");
 		requestEntity = new StringRequestEntity("CA", "appication/json", "UTF8");
 		put.setRequestEntity(requestEntity);
 		this.send(client, put, 204);
 
 		// FETCH ROW (VERIFY COLUMN INSERT)
-		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY);
+		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
 		this.send(client, get, 200);
 		body = get.getResponseBodyAsString();
-		assertEquals(
-				"{\"ADDR1\":\"1234 Fun St.\",\"STATE\":\"CA\",\"CITY\":\"Souderton.\"}",
-				body);
+		assertEquals("{\"ADDR1\":\"1235 Fun St.\",\"STATE\":\"CA\",\"COUNTY\":\"Montgomery\",\"CITY\":\"Souderton.\"}", body);
 		logger.debug(body);
 
 		// FETCH COLUMN
-		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY + "/CITY");
+		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY + "/CITY");
 		this.send(client, get, 200);
 		body = get.getResponseBodyAsString();
 		assertEquals("{\"CITY\":\"Souderton.\"}", body);
 		logger.debug(body);
 
 		// DELETE COLUMN
-		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY
-				+ "/" + KEY + "/CITY");
+		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY + "/CITY");
 		this.send(client, delete, 204);
 
 		// VERIFY COLUMN DELETE
-		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY);
+		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
 		this.send(client, get, 200);
 		body = get.getResponseBodyAsString();
-		assertEquals("{\"ADDR1\":\"1234 Fun St.\",\"STATE\":\"CA\"}", body);
+		assertEquals("{\"ADDR1\":\"1235 Fun St.\",\"STATE\":\"CA\",\"COUNTY\":\"Montgomery\"}", body);
 		logger.debug(body);
 
 		// DELETE ROW
-		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY
-				+ "/" + KEY);
+		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
 		this.send(client, delete, 200);
 
 		// VERIFY ROW DELETE
-		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/"
-				+ KEY);
+		get = new GetMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/" + KEY);
 		this.send(client, get, 204);
 		body = get.getResponseBodyAsString();
 		assertEquals(null, body);
 		logger.debug(body);
 
 		// CLEANUP : DROP COLUMN FAMILY
-		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY
-				+ "/");
+		delete = new DeleteMethod(BASE_URL + KEYSPACE + "/" + COLUMN_FAMILY + "/");
 		this.send(client, delete, -1);
 
 		// CLEANUP : DROP KEYSPACE
@@ -128,8 +129,7 @@ public class HttpTest {
 		this.send(client, delete, -1);
 	}
 
-	private void send(HttpClient client, HttpMethod method, int expect)
-			throws Exception {
+	private void send(HttpClient client, HttpMethod method, int expect) throws Exception {
 		try {
 			int result = client.executeMethod(method);
 			String body = method.getResponseBodyAsString();
