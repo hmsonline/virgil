c811e1621aebf083401fad089e89ee385d051fb2
Brian ONeill <bone@alumni.brown.edu>
2012-03-06 17:15:00 -0500

    Removed release shell script since it is no longer needed.

--- release.sh
+++
@@ -1,7 +0,0 @@
-# Releases Virgil
-mvn clean install 
-mvn hadoop:pack -f mapreduce/pom.xml -Dmaven.test.skip=true
-rm -rf release/assembly/mapreduce/jars
-mkdir release/assembly/mapreduce/jars
-cp mapreduce/target/hadoop-deploy/*.jar release/assembly/mapreduce/jars/.
-mvn assembly:assembly -f release/pom.xml -Dmaven.test.skip=true
